2e2fafec424d99e867cc1bed5cf2a6db7ed6d973
Aaron Kagan <aaronkagan@gmail.com>
2026-04-24 12:12:23 -0400

    adding to gameplay

--- MathGame.aaronkagan/Program.cs
+++ MathGame.aaronkagan/Program.cs
@@ -1,58 +1,82 @@
-﻿string[] levelsOfDifficulty = ["Easy", "Hard", "Very Hard"];
+﻿string[] levelsOfDifficulty = ["Easy", "Hard", "Genius"];
 string[] operations = ["Addition", "Subtraction", "Division", "Multiplication", "Random"];
 string[] operators = ["+", "-", "*", "/"];
 string[] previousGames = [];
 string levelOfDifficulty = "";
 var score = 0;
-var gameIsRunning = true;
+var gameIsRunning = true; 
+var startTime = DateTime.Now;
+
 
 while (gameIsRunning)
 {
-    var startTime = DateTime.Now;
     
-    string GetTimeElapsed()
+   
+
+    
+    Console.WriteLine($"The total game time was {GetTimeElapsed()}");
+    Console.WriteLine("Would you like to play again? y/n");
+    var playAgain = Console.ReadLine();
+    if (playAgain.Trim().ToLower() == "n")
     {
-        var currentTime = DateTime.Now;
-        var elapsedTime = currentTime - startTime;
-        var totalMinutes = (int)elapsedTime.TotalMinutes;
-        var remainingSeconds = elapsedTime.Seconds;
-        return $"{totalMinutes}:{(remainingSeconds < 10 ? "0" : "")}{remainingSeconds}";
+        gameIsRunning = false;
     }
 }
+
+string ShowMenu()
+{
+}
+
+string GetTimeElapsed()
+{
+    var currentTime = DateTime.Now;
+    var elapsedTime = currentTime - startTime;
+    var totalMinutes = (int)elapsedTime.TotalMinutes;
+    var remainingSeconds = elapsedTime.Seconds;
+    return $"{totalMinutes}:{(remainingSeconds < 10 ? "0" : "")}{remainingSeconds}";
+}
+
+int[] GetOperands()
+{
+    var firstOperand = Random.Shared.Next(1, 11);
+    var secondOperand = Random.Shared.Next(1, 11);
+
+    if (levelOfDifficulty == "Hard")
+    {
+        firstOperand = Random.Shared.Next(11, 101);
+        secondOperand = Random.Shared.Next(11, 101);
+    }
+    if (levelOfDifficulty == "Genius")
+    {
+        firstOperand = Random.Shared.Next(101, 1001);
+        secondOperand = Random.Shared.Next(101, 1001);
+    }
+
+    return [firstOperand, secondOperand];
+}
+
 int[] GenerateQuestion(string operation)
 {
     switch (operation)
     {
         case "Addition":
         {
-            var firstOperand = Random.Shared.Next(0, 11);
-            var secondOperand = Random.Shared.Next(0, 11);
-
-            if (levelOfDifficulty == "Hard")
-            {
-                firstOperand = Random.Shared.Next(0, 101);
-                secondOperand = Random.Shared.Next(0, 101);
-            }
-            if (levelOfDifficulty == "Very Hard")
-            {
-                firstOperand = Random.Shared.Next(0, 1001);
-                secondOperand = Random.Shared.Next(0, 1001);
-            }
-            
+            var firstOperand = GetOperands()[0];
+            var secondOperand = GetOperands()[1];
             var answer = firstOperand + secondOperand;
             return [firstOperand, secondOperand, answer];
         }
         case "Subtraction":
         {
-            var firstOperand = Random.Shared.Next(0, 11);
-            var secondOperand = Random.Shared.Next(0, 11);
+            var firstOperand = GetOperands()[0];
+            var secondOperand = GetOperands()[1];
             var answer = firstOperand - secondOperand;
             return [firstOperand, secondOperand, answer];
         }
         case "Multiplication":
         {
-            var firstOperand = Random.Shared.Next(0, 11);
-            var secondOperand = Random.Shared.Next(0, 11);
+            var firstOperand = GetOperands()[0];
+            var secondOperand = GetOperands()[1];
             var answer = firstOperand - secondOperand;
             return [firstOperand, secondOperand, answer];
         }
@@ -60,8 +84,8 @@ int[] GenerateQuestion(string operation)
         {
             while (true)
             {
-                var firstOperand = Random.Shared.Next(0, 11);
-                var secondOperand = Random.Shared.Next(0, 11);
+                var firstOperand = GetOperands()[0];
+                var secondOperand = GetOperands()[1];
 
                 if (firstOperand % secondOperand != 0)
                 {
