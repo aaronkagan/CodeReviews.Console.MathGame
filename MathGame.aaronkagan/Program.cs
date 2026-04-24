c90ae996df4afa00060581aa0318e4632acb92c7
Aaron Kagan <aaronkagan@gmail.com>
2026-04-24 11:52:47 -0400

    new structure

--- MathGame.aaronkagan/Program.cs
+++ MathGame.aaronkagan/Program.cs
@@ -1,1 +1,78 @@
-﻿Console.WriteLine("Hello World!");
+﻿string[] levelsOfDifficulty = ["Easy", "Hard", "Very Hard"];
+string[] operations = ["Addition", "Subtraction", "Division", "Multiplication", "Random"];
+string[] operators = ["+", "-", "*", "/"];
+string[] previousGames = [];
+string levelOfDifficulty = "";
+var score = 0;
+var gameIsRunning = true;
+
+while (gameIsRunning)
+{
+    var startTime = DateTime.Now;
+    
+    string GetTimeElapsed()
+    {
+        var currentTime = DateTime.Now;
+        var elapsedTime = currentTime - startTime;
+        var totalMinutes = (int)elapsedTime.TotalMinutes;
+        var remainingSeconds = elapsedTime.Seconds;
+        return $"{totalMinutes}:{(remainingSeconds < 10 ? "0" : "")}{remainingSeconds}";
+    }
+}
+int[] GenerateQuestion(string operation)
+{
+    switch (operation)
+    {
+        case "Addition":
+        {
+            var firstOperand = Random.Shared.Next(0, 11);
+            var secondOperand = Random.Shared.Next(0, 11);
+
+            if (levelOfDifficulty == "Hard")
+            {
+                firstOperand = Random.Shared.Next(0, 101);
+                secondOperand = Random.Shared.Next(0, 101);
+            }
+            if (levelOfDifficulty == "Very Hard")
+            {
+                firstOperand = Random.Shared.Next(0, 1001);
+                secondOperand = Random.Shared.Next(0, 1001);
+            }
+            
+            var answer = firstOperand + secondOperand;
+            return [firstOperand, secondOperand, answer];
+        }
+        case "Subtraction":
+        {
+            var firstOperand = Random.Shared.Next(0, 11);
+            var secondOperand = Random.Shared.Next(0, 11);
+            var answer = firstOperand - secondOperand;
+            return [firstOperand, secondOperand, answer];
+        }
+        case "Multiplication":
+        {
+            var firstOperand = Random.Shared.Next(0, 11);
+            var secondOperand = Random.Shared.Next(0, 11);
+            var answer = firstOperand - secondOperand;
+            return [firstOperand, secondOperand, answer];
+        }
+        case "Division":
+        {
+            while (true)
+            {
+                var firstOperand = Random.Shared.Next(0, 11);
+                var secondOperand = Random.Shared.Next(0, 11);
+
+                if (firstOperand % secondOperand != 0)
+                {
+                    continue;
+                }
+
+                var answer = firstOperand / secondOperand;
+                return [firstOperand, secondOperand, answer];
+                
+            }
+        }
+    }
+}
+
